e2ce5e714aa1d20022d97537c60c6f18c7c8227f
Sven Lussmann <sven.lussmann@gmail.com>
2024-12-12 13:42:34 +0000

    convert md to html

---
+++ convert.js
@@ -0,0 +1,22 @@
+import fs from 'fs';
+import {marked} from 'marked';
+
+// Lese die task-3.md Datei
+fs.readFile('task-3.md', 'utf8', (err, data) => {
+  if (err) {
+    console.error('Fehler beim Lesen der Datei:', err);
+    return;
+  }
+
+  // Konvertiere Markdown zu HTML
+  const htmlContent = marked(data);
+
+  // Schreibe die resultierende HTML-Datei
+  fs.writeFile('task-3.html', htmlContent, (err) => {
+    if (err) {
+      console.error('Fehler beim Schreiben der HTML-Datei:', err);
+    } else {
+      console.log('task-3.html wurde erfolgreich erstellt!');
+    }
+  });
+});
